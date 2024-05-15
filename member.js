bc5619f94bdaa5ed8573461af978662eaecb2003
Seifolah Ghaderi <sepelloo@gmail.com>
2024-05-15 09:30:23 +0330

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+﻿function skillsMember() {
+    return {
+        name: "skillsMember",
+        restrict: 'E',
+        templateUrl: 'app/components/member/skillsMember.html',
+        scope: {
+            skills: '='
+        }
+    };
+}
